452625dd43a28e0c64e1d9a1273f9ab54065a8b6
Egor Titov <etitov@ITSK-PF259QS8.localdomain>
2020-09-09 22:20:16 +0500

    lol1

---
+++ update.bat
@@ -0,0 +1,26 @@
+cd ..\10-java-functional-stream
+gradlew.bat build  -x test 
+cd ..\9-java-generics-collections
+gradlew.bat build  -x test 
+cd ..\8-java-io2
+gradlew.bat build  -x test 
+cd ..\1-java-intros
+gradlew.bat build  -x test 
+cd ..\7-java-io
+gradlew.bat build  -x test 
+cd ..\6-java-exceptions
+gradlew.bat build  -x test 
+cd ..\5-java-oop-principles
+gradlew.bat build  -x test 
+cd ..\4-java-oop-basic
+gradlew.bat build  -x test 
+cd ..\3-java-control-flow-and-arrays
+gradlew.bat build  -x test 
+cd ..\2-java-types
+gradlew.bat build  -x test 
+cd ..\javac
+gradlew.bat build  -x test 
+cd ..\slackbot 
+gradlew.bat build  -x test 
+cd ..\practice
+gradlew.bat build  -x test 
